25c7346d860159b02e980a3ca9f24b330acf75a5
gsistelos <gabrielsistelos@gmail.com>
2024-07-27 03:19:26 -0300

    add dotenvx

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
+import { configDotenv } from "@dotenvx/dotenvx";
+
 import express from "express";
 import type { Request, Response } from "express";
+
+configDotenv();
 
 const app = express();
 const port = process.env.PORT || 3000;
