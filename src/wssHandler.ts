4b27d590f43fe2ea09c130e21b045fffde484b08
gsistelos <gabrielsistelos@gmail.com>
2024-10-01 20:52:10 -0300

    websocket authentication with one use token

--- src/wssHandler.ts
+++ src/wssHandler.ts
@@ -1,6 +1,55 @@
 import type { WebSocket } from "ws";
+import redisClient from "./redisClient.js";
 
-export function wssHandler(ws: WebSocket) {
+function getQuery(url: string) {
+	const paramsIndex = url.indexOf("?");
+	if (paramsIndex === -1 || paramsIndex === url.length - 1) {
+		return undefined;
+	}
+
+	const params = url.slice(paramsIndex + 1);
+
+	return params.split("&");
+}
+
+function getParams(url: string) {
+	const query = getQuery(url);
+	if (!query) {
+		return undefined;
+	}
+
+	const result = new Array<string>();
+
+	for (const part of query) {
+		const param = part.split("=");
+
+		result.push(param[1]);
+	}
+
+	return result;
+}
+
+export async function wssHandler(ws: WebSocket, req: Request) {
+	const params = getParams(req.url);
+	if (!params) {
+		ws.close();
+		return;
+	}
+
+	const userId = params[0];
+	const wsToken = params[1];
+
+	const key = `ws-token-${userId}`;
+
+	const redisToken = await redisClient.get(key);
+
+	if (wsToken !== redisToken) {
+		ws.close();
+		return;
+	}
+
+	await redisClient.del(key);
+
 	ws.on("error", (err) => {
 		console.log(`Websocket error: ${err}`);
 	});
